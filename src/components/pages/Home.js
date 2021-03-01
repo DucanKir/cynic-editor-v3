3868a8ef1ac6f2ffb5c92f304c567250dd3f167a
Lana Kirilenko <svetlana.kirilenko@hotmail.com>
2021-03-01 14:25:51 +0000

    fix

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/free-solid-svg-icons'
-import '@fortawesome/react-fontawesome'
 
 import GameContainer from '../game/game-container'
 
